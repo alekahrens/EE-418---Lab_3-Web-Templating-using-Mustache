4ee574bbd0b2e9b6ef34e930abf191cec0f18128
Vxcaroda <davidvicaro@gmail.com>
2022-10-03 19:04:09 -0400

    I have checked the file “books.php” and made sure it now only contains code related to the rendering of the “page.mustache” template. There is no HTML or other unneccesary code in this file.

--- books.php
+++ books.php
@@ -5,9 +5,11 @@ include 'service.php';
 
 Mustache_Autoloader::register();
 
+//this is code that renders the template using Mustache
 $m = new Mustache_Engine(array('loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/templates')));
 
 $template = $m-> loadTemplate('page');
 
+//this is code that is related to fetching book data
 //echo $bookstemp->render();
 echo $template->render(array('books' => $_SESSION["service"]));
